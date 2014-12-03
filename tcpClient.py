45c4d8999ca57be7c9f6a63e2507092329f885cb
niwho <niwhocn@gmail.com>
2014-12-03 21:27:59 +0800

    ~

--- tcpClient.py
+++ tcpClient.py
@@ -8,6 +8,8 @@ monkey.patch_sys()
 
 import select
 import string 
+import random
+import logging
 
 def raw_input2(message):
     #sys.stdout.write(message)
@@ -30,7 +32,6 @@ class MyClient(object):
         self.userinputstr = ''
         pass
     
-    @xx
     def helloWorld(self):
         
         pass
@@ -57,10 +58,116 @@ class MyClient(object):
                     pre = cur
                 except Exception,e:
                     pass
-                
+
+from tcpServer import TestProtocolException,TestProtocol,RPCSocket
+
+class SocketRPCClient(object):
+    """ RPClient for the above Server.
+        Automaticaly reconnects to the target server (with "reconnect=True") 
+        but looses any results which hasn't been transfered on reconnect.
+    """
+
+    ## START Reconnecting feature,
+    # shameless borrowed from 
+    # twisted.i.p.ReconnectingClientFactory (Rene)
+    maxDelay = 3600
+    initialDelay = 1.0
+    factor = 2.7182818284590451
+    jitter = 0.11962656472
+
+    delay = initialDelay
+    retries = 0
+    maxRetries = None
+
+    continueTrying = True
+    isTrying = False
+    ## END Reconnecting
+
+    def __init__(self, address, rpctype,prototype, timeout=None, source_address=None, reconnect=False):
+        self.sock_args = [address, timeout, source_address]
+        self.rpctype=rpctype
+        self.prototype=prototype
+       
+
+        self.continueTrying = reconnect
+
+       
+
+        # Start connecting
+        self.connect()
+    def main_loop(self):
+        gevent.event.Event().wait()
+    def connect(self):
+        # Protocol specific
+        try:
+            self.socket = gevent.socket.create_connection(*self.sock_args)
+            self.rpc = self.rpctype(self.prototype,self.socket)
             
+            self.address = self.socket.getpeername()
+
+            gevent.spawn(self.rpc._read)
+        except Exception, e:
+            print e
+            self.connection_failed(e)
+
+    def connection_failed(self, reason):
+        #logging.error('Connection to %s:%s failed: %s' % (self.sock_args[0][0], self.sock_args[0][1], reason))
+
+        if self.continueTrying and not self.isTrying:
+            self._retry()
+
+    def connection_made(self):
+        logging.info('Connected to %s:%s' % self.address)
+
+    def connection_lost(self):
+        logging.info('Lost connection to %s:%s' % self.address)
+
+        if self.continueTrying and not self.isTrying:
+            self._retry()
+    
+    def helloworld(self):
+        return self.rpc.helloto_c().get()
+    def _retry(self):
+        self.isTrying = True
+
+        if self.connected.is_set():
+            self.isTrying = False
+            self.delay = self.initialDelay
+            self.retries = 0
+
+            if self.debug:
+                logging.debug("Successfully reconnected to %s:%s" % self.sock_args[0])
+            return
+
+        if not self.continueTrying:
+            if self.debug:
+                logging.debug("Abandoning connecting to %s:%s on explicit request" % self.sock_args[0])
+            return
+
+        self.retries += 1
+        if self.maxRetries is not None and (self.retries > self.maxRetries):
+            if self.debug:
+                logging.debug("Abandoning %s:%s after %d retries." % (self.sock_args[0][0], self.sock_args[0][1], self.retries))
+            return
+
+        self.delay = min(self.delay * self.factor, self.maxDelay)
+        if self.jitter:
+            self.delay = random.normalvariate(self.delay,
+                                              self.delay * self.jitter)
+
+        if self.debug:
+            logging.debug("%s:%s will retry in %d seconds" % (self.sock_args[0][0], self.sock_args[0][1], self.delay,))
+
+        self.connect()
+
+        gevent.spawn_later(self.delay, self._retry)
+     
 if __name__ == '__main__':
-    mc = MyClient()
+    client = SocketRPCClient(('192.168.12.21',6000),RPCSocket,TestProtocol)
+    print client.helloworld()
+    #print ('!!!!!!!!!!!!!!!!!!!')
+    gevent.sleep(0)
+    #mc = MyClient()
     
     #gevent.joinall([gevent.spawn(mc.loop),gevent.spawn(mc.getinput)])
-    gevent.joinall([gevent.spawn(mc.loop),gevent.spawn(mc.getinput)])
+    #gevent.joinall([gevent.spawn(mc.loop),gevent.spawn(mc.getinput)])
--- tcpServer.py
+++ tcpServer.py
@@ -8,6 +8,7 @@ Terminate the connection by terminating telnet (typically Ctrl-] and then 'quit'
 from __future__ import print_function
 from gevent.server import StreamServer
 import gevent
+import gevent.event
 import struct
 
 class MyHandler(object):
@@ -40,19 +41,18 @@ class MyHandler(object):
         socket.sendall('Welcome to the echo server! Type quit to exit.\r\n')
         # using a makefile because we want to use readline()
         try:
-            r = gevent.spawn(self.reader,socket)
-            w = gevent.spawn(self.writer,socket)
-            gevent.joinall([r, w])
+            gevent.spawn(self.reader,socket)
+            gevent.spawn(self.writer,socket)
         finally:
             #del(users[name])
             #broadcast('## %s left the chat.' % name)
             print ('close',socket,self.socks[socket])
             self.socks.pop(socket)
 
-class ISCMProtocolException(Exception):
+class TestProtocolException(Exception):
     def __init__(self):
         pass
-class ISCMProtocol(object):
+class TestProtocol(object):
     def __init__(self,socket):
         self.socket = socket
         pass
@@ -67,6 +67,12 @@ class ISCMProtocol(object):
             data+=part
         return data
     
+    def _sendSized(self,data):
+        size= len(data)
+        sended=0
+        while sended<size:
+            sended+=self.socket.send(data[sended:])#gevent 在连接失去时，这个接口仍出异常？
+            
     def readStream(self):
         try:
             #网络字节序，
@@ -75,7 +81,7 @@ class ISCMProtocol(object):
             datalen,reqcode,seqcode,replyormethlen= struct.unpack('!IBIi',self._readSized(headlen))
             datalen-=headlen
             if datalen<1:
-                raise ISCMProtocolException()
+                raise TestProtocolException()
             data = self._readSized(datalen)
             
             if reqcode == 0:
@@ -92,26 +98,40 @@ class ISCMProtocol(object):
                 pass
            
             else :
-                raise ISCMProtocolException()
+                raise TestProtocolException()
         except Exception,e:
             print(e)
-            raise ISCMProtocolException()
+            raise TestProtocolException()
         
         
         pass
     
-    def writeStream(self,**kw):
+    def writeStream(self,fmat,*parms):
         try:
             pass
-        except Exception:
-            pass
+            data = struct.pack(fmat,*parms)
+            self._sendSized(data)
+        except Exception,e:
+            print(e)
+            raise TestProtocolException()
         pass
     
 class RPCSocket(object):
     def __init__(self,protoclass,socket):
-        self.proto = protoclass(socket)
-        self.socket = socket
-        pass
+        try:
+            self.proto = protoclass(socket)
+            self.socket = socket
+        
+            self.seqcall={}
+            self.seqid = 0
+        except Exception,e:
+            print(e)
+            
+    
+
+            
+    def isConnected(self):
+        return False
     
     def _read(self):
         
@@ -122,50 +142,116 @@ class RPCSocket(object):
                     self.OnReply(seqcode, relpycode_or_methodname, params)
                     pass
                 elif reqcode == 1:
+                    print(seqcode,relpycode_or_methodname,params)
                     self.OnCall(seqcode,relpycode_or_methodname,params)
-                    pass
-                elif reqcode == 2:
-                    self.OnPostingCall(seqcode,relpycode_or_methodname,params)
                     pass
                 else:
                     print('无法识别的请求码')
-            except ISCMProtocolException,ee:
+            except TestProtocolException,ee:
                 print(ee)
+                break
             except Exception:
+                print(ee)
+                break
                 pass
             #派发请求（可能是结果通知）
         pass
     
-    #这个接口是否有存在必要性
-    def _write(self,*args):
-        
-        while True:
-            self.proto.writeStream(*args)
-        pass
+    #这个接口是否有存在必要性 noneed
+    #def _write(self,*args):
+    #    
+    #    while True:
+    #        self.proto.writeStream(*args)
+    #    pass
     
     #call 有2中，一种是调用write（发送请求），一种是被调用read（接受请求）
     def OnCall(self,seqcode,methodname,params):
         if not hasattr(self,methodname):
             pass
-        
-        pass
-    
-    #同上
-    def OnPostingCall(self,seqcode,methodname,params):
-        pass
+        try:
+            bounded_func = getattr(self,methodname)
+            #bounded_func.seqcode=seqcode
+            self.seqcode=seqcode
+            print (params)
+            bounded_func()
+           
+        except Exception,e:
+            print(e,'函数调用失败')
+            pass
     
     #发送处理结果write
     def OnReply(self,seqcode,relpycode,params):
+        
+        self.seqcall[seqcode].set((relpycode,params))
+        del self.seqcall[seqcode]
         pass
     
-    ###methods####
-    def helloworld(self):
-        print('hello world!')
+    def call(self):
+        
+        pass
     
+    def reply(self):
+        
+        pass
+    ###methods####
+    #call
+    def helloworld_s(self):
+        print('hello world! from %s'%(self.socket.getsockname(),))
+        data='hello reply from %s'%(self.socket.getsockname(),)
+        fmat='!IBIi%ds'%(len(data),)
+        self.proto.writeStream(fmat,13+len(data),0,self.seqcode,0,data)
+    
+    #posting or not posting 由调用端确定  不影响服务器端
+    def helloworldserver(self):
+        print('hello from clinet')
+        
+    #method 
+    def helloto_c(self):
+        try:
+            self.seqid=self.seqid%100+1
+            data='hello call from %s'%(self.socket.getsockname(),)
+            fmat='!IBIi%ds%ds'%(len('helloworld_s'),len(data))
+            self.proto.writeStream(fmat,13+len(data)+len('helloworld_s'),1,self.seqid,len('helloworld_s'),'helloworld_s',data)
+            syncFin = gevent.event.AsyncResult()
+            self.seqcall[self.seqid] = syncFin
+            
+        except Exception,e:
+            print(e)
+            return (-1,)
+        return syncFin
+    
+    #post method
+    def hello_c_posting(self):
+        try:
+            self.seqid=self.seqid%100+1
+            data='hello call from %s'%(self.socket.getsockname(),)
+            fmat='!IBIi%ds%ds'%(len(self.hellotoinvoker.__name__),len(data))
+            self.proto.writeStream(fmat,13+len(data),1,self.seqid,len('helloworld_s'),'helloworld_s',data)
+        except Exception,e:
+            print (e)
+            return (-1,)
+        return (0,)
+    
+class SocketRPCServer(StreamServer):
+    def __init__(self, listener, rpctype,prototype, backlog=None, spawn='default'):
+        StreamServer.__init__(self, listener, backlog=backlog, spawn=spawn)
+        self.rpcmodel = rpctype
+        self.proto =prototype
+
+    def handle(self, socket, address):
+        """ Start the socket handlers
+            self.protocol.handle_write and
+            self.protocol.handle_read.
+        """
+        realrpc=self.rpcmodel(self.proto,socket)
+        print("connection from %s"%(str(address),))
+        realrpc._read()
+
+
 if __name__ == '__main__':
     # to make the server use SSL, pass certfile and keyfile arguments to the constructor
-    server = StreamServer(('0.0.0.0', 6000), MyHandler())
+    server = SocketRPCServer(('0.0.0.0', 6000), RPCSocket,TestProtocol)
     # to start the server asynchronously, use its start() method;
     # we use blocking serve_forever() here because we have no other jobs
-    print('Starting echo server on port 6000')
+    print('Starting server on port 6000')
     server.serve_forever()
